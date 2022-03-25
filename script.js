16419d5b42e07545ad141f85b1e8524fcfaf84a1
Jeff Lin <thejefflin@gmail.com>
2022-03-25 01:24:32 -0400

    Initialize JS events

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+function myFunction() {
+    alert("SITE IS TEMPORARY OUT OF SERVICE :(");
+}
 
-document.querySelector("body").appendChild(h2);
